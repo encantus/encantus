61630e2cfe20a5c0c6f7ddaa47388c40d3f609fb
bmlevchenko <bmlevchenko@gmail.com>
2016-01-01 22:43:19 +0200

    Project skeleton

---
+++ src/main/java/org/encantus/EncantusApplication.java
@@ -0,0 +1,12 @@
+package org.encantus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncantusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncantusApplication.class, args);
+	}
+}
